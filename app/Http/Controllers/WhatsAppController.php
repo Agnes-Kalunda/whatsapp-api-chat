5fe7a8a9fd184069e663bf33cfb68bac2bc8cce2
Agnes <agypeter97@gmail.com>
2024-10-26 17:40:00 +0300

    define constructor

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Chat\WhatsappIntegration\WhatsApp;
 
 class WhatsAppController extends Controller
 {
     //
+    protected $whatsapp;
+
+    public function __construct(Whatsapp $whatsapp){
+        $this->whatsapp = $whatsapp;
+
+    }
+
+    
 }
